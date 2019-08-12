034d3b65dbd5d27051b0907006e102449d88b970
kargo <karenrgo@gmail.com>
2019-08-11 23:03:07 -0700

    pokemon can show their butts

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,14 +2,24 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state = {
+    imgState: true
+  }
+
+  onToggleImg = () => {
+    this.setState({ imgState: !this.state.imgState })
+  }
+
   render() {
     let pokemon = this.props.pokemon
+    let img
+    this.state.imgState ? img = pokemon.sprites.front : img = pokemon.sprites.back
 
     return (
       <Card>
         <div>
-          <div className="image">
-            <img src={pokemon.sprites.front} />
+          <div className="image" onClick={this.onToggleImg}>
+            <img src={img} />
           </div>
           <div className="content">
             <div className="header">{pokemon.name}</div>
