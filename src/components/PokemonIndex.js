3846d01b237207604ca65e778b5b304019ffcf2f
kargo <karenrgo@gmail.com>
2019-08-11 23:32:59 -0700

    search bar works

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -10,16 +10,35 @@ class PokemonPage extends React.Component {
   constructor() {
     super()
     this.state = {
-      all: []
+      all: [],
+      search: ""
     }
   }
 
   componentDidMount() {
+    this.getPokemon()
+  }
+
+  getPokemon = () => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
-        this.setState({ all: data })
+        this.setState({ all: data, save: data })
       })
+  }
+
+  searchPokemon = (e) => {
+    this.setState({ search: e.target.value })
+    this.filterPokemon()
+  }
+
+  filterPokemon = () => {
+    this.setState(prevState => {
+      let allPokemon = prevState.save
+      let str = prevState.search
+      let newPokemon = allPokemon.filter(pokemon => pokemon.name.includes(str))
+      return { all: newPokemon }
+    })
   }
 
   render() {
@@ -27,7 +46,7 @@ class PokemonPage extends React.Component {
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={(e) => this.searchPokemon(e)} showNoResults={false} />
         <br />
         <PokemonCollection all={this.state.all} />
         <br />
